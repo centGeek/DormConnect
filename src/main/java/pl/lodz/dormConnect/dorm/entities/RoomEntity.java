b49bf123e9ec91d11724c95ed0f324a43412e004
qmacdom <qmacdom@gmail.com>
2025-04-02 17:37:58 +0200

    All UTs are RED (xD) próba pierwszego testu. Dodanie get

--- src/main/java/pl/lodz/dormConnect/dorm/entities/RoomEntity.java
+++ src/main/java/pl/lodz/dormConnect/dorm/entities/RoomEntity.java
@@ -1,7 +1,15 @@
 package pl.lodz.dormConnect.dorm.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name = "DormRooms")
 public class RoomEntity {
