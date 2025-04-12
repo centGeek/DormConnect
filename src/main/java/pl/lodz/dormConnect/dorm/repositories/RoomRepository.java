d0d1ff75eceef0116b61f31414583ffd5c81f0dc
qmacdom <qmacdom@gmail.com>
2025-04-12 17:55:10 +0200

    poprawiono błąd z dialektem

--- src/main/java/pl/lodz/dormConnect/dorm/repositories/RoomRepository.java
+++ src/main/java/pl/lodz/dormConnect/dorm/repositories/RoomRepository.java
@@ -1,13 +1,14 @@
 package pl.lodz.dormConnect.dorm.repositories;
 
 import jakarta.persistence.Id;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.lodz.dormConnect.dorm.entities.RoomEntity;
 
 @Repository
-public interface RoomRepository extends CrudRepository<RoomEntity, Long> {
+public interface RoomRepository extends JpaRepository<RoomEntity, Long> {
 
 
-    void countRoomEntity();
+//    void countRoomEntity();
 }
