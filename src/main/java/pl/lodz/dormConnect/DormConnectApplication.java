0203b86b7d330fd69d901dff89cf1484c72ed0c7
247638 Centkowski Łukasz <247638@edu.p.lodz.pl>
2025-04-14 13:49:31 +0200

    CommandLineRunner added

--- src/main/java/pl/lodz/dormConnect/DormConnectApplication.java
+++ src/main/java/pl/lodz/dormConnect/DormConnectApplication.java
@@ -3,19 +3,13 @@ package pl.lodz.dormConnect;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import pl.lodz.dormConnect.security.repository.UserRepository;
 
 @SpringBootApplication
 @Slf4j
 public class DormConnectApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(DormConnectApplication.class, args);
-        UserRepository bean = run.getBean(UserRepository.class);
-
-        //log that users are created
-        log.info("Created users: " + bean.findAll());
+        SpringApplication.run(DormConnectApplication.class, args);
     }
 
 }
