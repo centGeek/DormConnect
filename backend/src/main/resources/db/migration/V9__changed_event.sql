f7c9551485b23c33bc72dd9d333ebb4faa0fe265
AbsZeroO <138863520+AbsZeroO@users.noreply.github.com>
2025-04-28 19:13:50 +0200

    wywaliłem to co nie było moje XD

--- backend/src/main/resources/db/migration/V9__changed_event.sql
+++ backend/src/main/resources/db/migration/V9__changed_event.sql
@@ -1,28 +1,8 @@
-ALTER TABLE dorm_rooms_room_assigns
-DROP
-CONSTRAINT fk_dorroorooass_on_room_assign_entity;
-
-ALTER TABLE dorm_rooms_room_assigns
-DROP
-CONSTRAINT fk_dorroorooass_on_room_entity;
-
 ALTER TABLE event_organizer
 DROP
 CONSTRAINT fk_event_organizer_on_event_entity;
 
-ALTER TABLE grouped_rooms_entity_rooms
-DROP
-CONSTRAINT fk_grorooentroo_on_grouped_rooms_entity;
-
-ALTER TABLE grouped_rooms_entity_rooms
-DROP
-CONSTRAINT fk_grorooentroo_on_room_entity;
-
 ALTER TABLE event
     ADD organizer_id BIGINT;
 
-DROP TABLE dorm_rooms_room_assigns CASCADE;
-
 DROP TABLE event_organizer CASCADE;
-
-DROP TABLE grouped_rooms_entity_rooms CASCADE;
