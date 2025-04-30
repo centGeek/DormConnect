71fafdcc53003c2400f106718481d615e0ae2046
Jakub Samek <samek.jakub02@gmail.com>
2025-04-30 20:19:37 +0200

    chat.css adjusted

--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -1,11 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import Template from '../Template/Template.tsx';
 import './Home.css';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 function Home() {
-    const [counter, setCounter] = useState(0);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -24,8 +23,6 @@ function Home() {
             <div className="home-container">
                 <h1>Welcome in Dorm Connect</h1>
                 <h3>It's an application designed to manage your dormitory</h3>
-                <h4>You can play with this counter below</h4>
-                <button className="home-button" onClick={() => setCounter(counter + 1)}>{counter}</button>
             </div>
         </Template>
     );
