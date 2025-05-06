cdc200d59866a09edf2ef663baa734d3dfdab40b
Jakub Samek <samek.jakub02@gmail.com>
2025-05-06 13:12:50 +0200

    Nie wiem co tu sie dzieje

--- backend/src/main/java/pl/lodz/dormConnect/schedule/entity/CommonRoom.java
+++ backend/src/main/java/pl/lodz/dormConnect/schedule/entity/CommonRoom.java
@@ -2,20 +2,32 @@ package pl.lodz.dormConnect.schedule.entity;
 
 
 import jakarta.persistence.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table (name = "common_room")
 public class CommonRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "common_room_id")
     private Long id;
+
+    @Column (name = "common_room_type")
     private String type;
+
+    @Column (name = "capacity")
     private int capacity;
+
+    @Column (name = "floor")
     private int floor;
+
+    @Column (name = "max_time_you_can_stay")
     private int maxTimeYouCanStay;
+
+    @Column (name = "is_active")
     private boolean active = true;
 
-    @OneToMany
+    @OneToMany(mappedBy = "commonRoom")
     private List<CommonRoomAssigment> commonRoomAssign = new ArrayList<>();
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import { useContext, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './Login/Login.tsx';
 import Chat from './Chat/Chat.tsx';
 import Home from './Home/Home.tsx';
 import EventsCreate from './Events/EventsCreate.tsx';
 import Events from './Events/Events.tsx';
-import { UserContext, UserProvider } from './Context/UserContext.tsx';
+import { UserContext } from './Context/UserContext.tsx';
 import Rooms from './Rooms/RoomPage.tsx';
 import AdminEvents from './Events/AdminEvents.tsx';
 import EventsEdit from './Events/EventsEdit.tsx';
