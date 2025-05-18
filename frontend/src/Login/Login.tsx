e59183473c7e625d44121214c35f9897949d7649
AbsZeroO <138863520+AbsZeroO@users.noreply.github.com>
2025-05-18 16:55:50 +0200

    Merge with main. Fix Login Page

--- frontend/src/Login/Login.tsx
+++ frontend/src/Login/Login.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useContext } from 'react';
 import './Login.css';
-import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../Context/UserContext.tsx';
 
 function Login() {
