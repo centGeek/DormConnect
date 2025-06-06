e06b467bbbd96c6ee38c340ebaa4bcccd7068e9c
AbsZeroO <138863520+AbsZeroO@users.noreply.github.com>
2025-06-06 12:40:29 +0200

    smart-bin works but without security

---
+++ backend/smart-bin/src/test/java/pl/lodz/smartbin/SmartBinApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.smartbin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
