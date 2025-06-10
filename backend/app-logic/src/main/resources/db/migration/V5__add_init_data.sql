194a1517ebdb9a4fcbd1dc62d40a5ca7f4dcc064
Piotr Polakowski <p.polakowski@proton.me>
2025-06-10 09:55:06 +0200

    Flyway migration

--- backend/app-logic/src/main/resources/db/migration/V5__add_init_data.sql
+++ backend/app-logic/src/main/resources/db/migration/V5__add_init_data.sql
@@ -4,13 +4,13 @@ values
     (2, 'MANAGER'),
     (3, 'STUDENT');
 
-insert into user_profile (user_name, email, password, is_active, role_id)
-values ('admin', 'admin@edu.p.lodz.pl', '$2a$12$TwQsp1IusXTDl7LwZqL0qeu49Ypr6vRdEzRq2vAsgb.zvOtrnzm5G', true, 1);
+insert into user_profile (user_name, uuid, email, password, is_active, role_id)
+values ('admin', '27de1019-5fff-4f68-ad1c-854f77ded2ed', 'admin@edu.p.lodz.pl', '$2a$12$TwQsp1IusXTDl7LwZqL0qeu49Ypr6vRdEzRq2vAsgb.zvOtrnzm5G', true, 1);
 
 
-insert into user_profile (user_name, email, password, is_active, role_id)
-values ('manager', 'manager@edu.p.lodz.pl', '$2a$12$TwQsp1IusXTDl7LwZqL0qeu49Ypr6vRdEzRq2vAsgb.zvOtrnzm5G', true, 2);
+insert into user_profile (user_name, uuid, email, password, is_active, role_id)
+values ('manager', '85ed70c6-698b-47e1-88b3-788a4c7aa5ae', 'manager@edu.p.lodz.pl', '$2a$12$TwQsp1IusXTDl7LwZqL0qeu49Ypr6vRdEzRq2vAsgb.zvOtrnzm5G', true, 2);
 
 
-insert into user_profile (user_name, email, password, is_active, role_id)
-values ('student_debil', 'student_debil@edu.p.lodz.pl', '$2a$12$TwQsp1IusXTDl7LwZqL0qeu49Ypr6vRdEzRq2vAsgb.zvOtrnzm5G', true, 3);
+insert into user_profile (user_name, uuid, email, password, is_active, role_id)
+values ('student_debil', '85ed70c6-698b-47e1-88b3-788a4c7aa5ae', 'student_debil@edu.p.lodz.pl', '$2a$12$TwQsp1IusXTDl7LwZqL0qeu49Ypr6vRdEzRq2vAsgb.zvOtrnzm5G', true, 3);
