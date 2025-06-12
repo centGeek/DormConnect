8f8447d4633fe53b2d2aa10f3cc4344e98db5362
AbsZeroO <138863520+AbsZeroO@users.noreply.github.com>
2025-06-12 20:04:19 +0200

    Starting events

--- frontend/src/setup-proxy.js
+++ frontend/src/setup-proxy.js
@@ -2,16 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/auth',
+    '/',
     createProxyMiddleware({
-      target: 'http://localhost:8091',
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:8081',
+      target: 'http://localhost:8000',
       changeOrigin: true,
     })
   );
