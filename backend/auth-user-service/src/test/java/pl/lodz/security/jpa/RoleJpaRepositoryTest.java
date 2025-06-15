54335371ef107096872ed81bcfa4f44637643524
AbsZeroO <138863520+AbsZeroO@users.noreply.github.com>
2025-06-15 14:06:11 +0200

    All test works

--- backend/auth-user-service/src/test/java/pl/lodz/security/jpa/RoleJpaRepositoryTest.java
+++ backend/auth-user-service/src/test/java/pl/lodz/security/jpa/RoleJpaRepositoryTest.java
@@ -1,47 +1,47 @@
-package pl.lodz.security.jpa;
+    package pl.lodz.security.jpa;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
-import pl.lodz.entity.RoleEntity;
-import pl.lodz.repository.jpa.RoleJpaRepository;
-import pl.lodz.repository.jpa.UserRepository;
-import pl.lodz.security.fixtures.StudentFixture;
+    import org.junit.jupiter.api.Assertions;
+    import org.junit.jupiter.api.Test;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+    import org.springframework.boot.test.context.SpringBootTest;
+    import org.springframework.test.context.ActiveProfiles;
+    import org.springframework.transaction.annotation.Transactional;
+    import pl.lodz.entity.RoleEntity;
+    import pl.lodz.repository.jpa.RoleJpaRepository;
+    import pl.lodz.repository.jpa.UserRepository;
+    import pl.lodz.security.fixtures.StudentFixture;
 
-import java.util.Optional;
+    import java.util.Optional;
 
-@SpringBootTest
-@ActiveProfiles("test")
-@Transactional
-public class RoleJpaRepositoryTest {
+    @SpringBootTest
+    @ActiveProfiles("test")
+    @Transactional
+    public class RoleJpaRepositoryTest {
 
-    @Autowired
-    private RoleJpaRepository roleJpaRepository;
+        @Autowired
+        private RoleJpaRepository roleJpaRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+        @Autowired
+        private UserRepository userRepository;
 
-    @Test
-    public void thatRoleIsRetrievedProperlyByRoleName() {
-        // given
-        RoleEntity studentRole = new RoleEntity("STUDENT");
-        roleJpaRepository.saveAndFlush(studentRole);
+        @Test
+        public void thatRoleIsRetrievedProperlyByRoleName() {
+            // given
+            RoleEntity studentRole = new RoleEntity("STUDENT");
+            roleJpaRepository.saveAndFlush(studentRole);
 
-        var studentEntity = StudentFixture.anyStudentEntity(studentRole);
-        var userEntity = studentEntity.getUser();
-        userRepository.saveAndFlush(userEntity);
+            var studentEntity = StudentFixture.anyStudentEntity(studentRole);
+            var userEntity = studentEntity.getUser();
+            userRepository.saveAndFlush(userEntity);
 
-        // when
-        Optional<RoleEntity> foundRoleOpt = Optional.ofNullable(roleJpaRepository.findByRole("STUDENT"));
+            // when
+            Optional<RoleEntity> foundRoleOpt = Optional.ofNullable(roleJpaRepository.findByRole("STUDENT"));
 
-        // then
-        Assertions.assertTrue(foundRoleOpt.isPresent(), "Role STUDENT should be present");
-        RoleEntity foundRole = foundRoleOpt.get();
+            // then
+            Assertions.assertTrue(foundRoleOpt.isPresent(), "Role STUDENT should be present");
+            RoleEntity foundRole = foundRoleOpt.get();
 
-        Assertions.assertEquals(studentRole.getRoleName(), foundRole.getRoleName());
+            Assertions.assertEquals(studentRole.getRoleName(), foundRole.getRoleName());
+        }
     }
-}
--- backend/chat-service/src/test/java/pl/lodz/chatservice/ChatServiceApplicationTests.java
+++ backend/chat-service/src/test/java/pl/lodz/chatservice/ChatServiceApplicationTests.java
@@ -2,10 +2,13 @@ package pl.lodz.chatservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import pl.lodz.chatservice.config.PostgresContainerConfig;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(PostgresContainerConfig.class)
 class ChatServiceApplicationTests {
 
     @Test
